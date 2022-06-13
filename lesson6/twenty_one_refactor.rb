ba6854cc62e689418328a826e02f14d9464b8c59
EC2 Default User <ec2-user@ip-172-31-43-241.ec2.internal>
2022-06-13 15:07:23 +0000

    refactor twenty_one

--- lesson6/twenty_one_refactor.rb
+++ lesson6/twenty_one_refactor.rb
@@ -8,7 +8,7 @@ end
 
 def initialize_deck
   deck = []
-  4.times do |num|
+  4.times do
     2.upto(10) { |num| deck << num.to_s }
     FACE_CARDS.each { |face| deck << face }
   end
@@ -17,6 +17,12 @@ end
 
 def deal!(hand, deck)
   hand << deck.pop
+end
+
+def initialize_hand(deck)
+  hand = []
+  2.times { deal!(hand, deck) }
+  hand
 end
 
 def total(hand)
@@ -30,18 +36,15 @@ def total(hand)
   total
 end
 
-def display_result(player_hand, dealer_hand, player_total, dealer_total, score)
+def display_hands(player_hand, dealer_hand, player_total, dealer_total)
   puts "=============="
   prompt "Dealer has #{read_hand(dealer_hand)}, for a total of: #{dealer_total}"
   prompt "Player has #{read_hand(player_hand)}, for a total of: #{player_total}"
   puts "=============="
-  if player_total > 21
-    prompt "You busted! Dealer wins."
-    prompt "The score is: Player #{score[0]}, Dealer #{score[1] += 1}"
-  elsif dealer_total > 21
-    prompt "Dealer busts! You win."
-    prompt "The score is: Player #{score[0] += 1}, Dealer #{score[1]}"
-  elsif player_total > dealer_total
+end
+
+def display_result(score, player_total, dealer_total)
+  if player_total > dealer_total
     prompt "You win!"
     prompt "The score is: Player #{score[0] += 1}, Dealer #{score[1]}"
   elsif dealer_total > player_total
@@ -51,17 +54,28 @@ def display_result(player_hand, dealer_hand, player_total, dealer_total, score)
     prompt "It's a tie!"
     prompt "The score is: Player #{score[0]}, Dealer #{score[1]}"
   end
-  prompt "#{who_won?(score)} reaches 5 points first!" if winning_score?(score)
+  prompt "#{winner?(score)} reaches 5 points first!" if winner?(score)
   pause
 end
 
-def winning_score?(score)
-  score[0] >= 5 || score[1] >= 5
+def display_busted(score, player_total)
+  if player_total > 21
+    prompt "You busted! Dealer wins."
+    prompt "The score is: Player #{score[0]}, Dealer #{score[1] += 1}"
+  else
+    prompt "Dealer busts! You win."
+    prompt "The score is: Player #{score[0] += 1}, Dealer #{score[1]}"
+  end
+  prompt "#{winner?(score)} reaches 5 points first!" if winner?(score)
+  pause
 end
 
-def who_won?(score)
-  return "Player" if score [0] >= 5
-  "Dealer"
+def winner?(score)
+  if score[0] >= 5
+    'Player'
+  elsif score[1] >= 5
+    'Dealer'
+  end
 end
 
 def pause
@@ -75,11 +89,15 @@ def read_hand(hand)
   last = temp.pop
   temp.join(', ') << " and #{last}"
 end
-  
-def main_game(score, deck = initialize_deck, player_hand = [], dealer_hand = [], 
-  player_total = 0, dealer_total = 0)
-  2.times { deal!(player_hand, deck) }
-  2.times { deal!(dealer_hand, deck) }
+
+score = [0, 0]
+prompt "Welcome to Twenty_One!"
+prompt "Find the rules at: https://bargames101.com/21-card-game-rule/"
+pause
+loop do
+  deck = initialize_deck
+  player_hand = initialize_hand(deck)
+  dealer_hand = initialize_hand(deck)
   player_total = total(player_hand)
   dealer_total = total(dealer_hand)
   prompt "Dealer has: #{dealer_hand[0]} and ?"
@@ -94,7 +112,8 @@ def main_game(score, deck = initialize_deck, player_hand = [], dealer_hand = [],
       player_total = total(player_hand)
       pause
       if player_total > 21
-        display_result(player_hand, dealer_hand, player_total, dealer_total, score)
+        display_hands(player_hand, dealer_hand, player_total, dealer_total)
+        display_busted(score, player_total)
         break
       end
       prompt "You now have: #{read_hand(player_hand)}"
@@ -104,30 +123,27 @@ def main_game(score, deck = initialize_deck, player_hand = [], dealer_hand = [],
       prompt "Dealer Turn..."
       loop do
         break if dealer_total >= DEALER_STAY
-        prompt "Dealer hits at #{dealer_total}"
+        prompt "Dealer hits"
         deal!(dealer_hand, deck)
         dealer_total = total(dealer_hand)
       end
-      pause
       prompt "Dealer stays" unless dealer_total > 21
-      display_result(player_hand, dealer_hand, player_total, dealer_total, score)
+      pause
+      display_hands(player_hand, dealer_hand, player_total, dealer_total)
+      if dealer_total > 21
+        display_busted(score, player_total)
+      else
+        display_result(score, player_total, dealer_total)
+      end
       break
     else
       prompt "Please enter either hit or stay"
     end
   end
-end
-
-score = [0, 0]
-prompt "Welcome to Twenty_One!"
-prompt "Find the rules at: https://bargames101.com/21-card-game-rule/"
-pause
-loop do
-  main_game(score)
-  break if winning_score?(score)
-  prompt "Press enter to continue or type stop to exit"
+  break if winner?(score)
+  prompt "Press enter to continue or type exit to stop"
   answer = gets.chomp.downcase
-  break if answer.start_with?('s')
+  break if answer.start_with?('e')
   system 'clear'
 end
 prompt "Thank you for playing Twenty One! Goodbye!"
