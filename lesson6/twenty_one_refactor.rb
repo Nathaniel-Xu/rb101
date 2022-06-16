92fdaf5b6aa4c1de4f9348c9ba983eecd7f70678
EC2 Default User <ec2-user@ip-172-31-43-241.ec2.internal>
2022-06-16 00:52:37 +0000

    Twenty_one_refactor

--- lesson6/twenty_one_refactor.rb
+++ lesson6/twenty_one_refactor.rb
@@ -1,16 +1,32 @@
+SUITS = %w(Hearts Diamonds Spades Clubs)
 FACE_CARDS = %w(Queen King Jack Ace)
 GAME_SIZE = 21
 DEALER_STAY = 17
+GAMES_TO_WIN = 5
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def pause
+  prompt "Press enter for next screen..."
+  gets
+  system "clear"
+end
+
+def display_introduction
+  system 'clear'
+  prompt "Welcome to Twenty_One!"
+  prompt "Find the rules at: https://bargames101.com/21-card-game-rule/"
+  prompt "First to #{GAMES_TO_WIN} wins!"
+  pause
+end
+
 def initialize_deck
   deck = []
-  4.times do
-    2.upto(10) { |num| deck << num.to_s }
-    FACE_CARDS.each { |face| deck << face }
+  SUITS.each do |suit|
+    2.upto(10) { |num| deck << [num, suit] }
+    FACE_CARDS.each { |face| deck << [face, suit] }
   end
   deck.shuffle
 end
@@ -25,126 +41,200 @@ def initialize_hand(deck)
   hand
 end
 
-def total(hand)
+def initialize_game_data
+  {
+    player: {
+      cards: nil,
+      total: nil
+    },
+    dealer: {
+      cards: nil,
+      total: nil
+    },
+    score: {
+      player: 0,
+      dealer: 0
+    }
+  }
+end
+
+def populate!(game_data, deck)
+  game_data[:player][:cards] = initialize_hand(deck)
+  game_data[:dealer][:cards] = initialize_hand(deck)
+  game_data[:player][:total] = total(game_data[:player][:cards])
+  game_data[:dealer][:total] = total(game_data[:dealer][:cards])
+end
+
+def busted?(player)
+  player[:total] > GAME_SIZE
+end
+
+def total(cards)
   total = 0
-  non_ace_cards = hand.select { |card| card unless card == 'Ace' }
-  aces = hand.size - non_ace_cards.size
+  non_ace_cards = cards.select { |card| card unless card[0] == 'Ace' }
+  aces = cards.size - non_ace_cards.size
   non_ace_cards.each do |card|
-    total += FACE_CARDS.include?(card) ? 10 : card.to_i
+    total += FACE_CARDS.include?(card[0]) ? 10 : card[0]
   end
-  aces.times { total += total + 11 > 21 ? 1 : 11 }
+  aces.times { total += total + 11 > GAME_SIZE ? 1 : 11 }
   total
 end
 
-def display_hands(player_hand, dealer_hand, player_total, dealer_total)
-  puts "=============="
-  prompt "Dealer has #{read_hand(dealer_hand)}, for a total of: #{dealer_total}"
-  prompt "Player has #{read_hand(player_hand)}, for a total of: #{player_total}"
-  puts "=============="
+def increment_total!(player)
+  player[:total] = total(player[:cards])
 end
 
-def display_result(score, player_total, dealer_total)
-  if player_total > dealer_total
-    prompt "You win!"
-    prompt "The score is: Player #{score[0] += 1}, Dealer #{score[1]}"
-  elsif dealer_total > player_total
-    prompt "Dealer wins."
-    prompt "The score is: Player #{score[0]}, Dealer #{score[1] += 1}"
-  else
-    prompt "It's a tie!"
-    prompt "The score is: Player #{score[0]}, Dealer #{score[1]}"
-  end
-  prompt "#{match_winner(score)} reaches 5 points first!" if match_winner(score)
-  pause
+def read_cards(cards)
+  result = '['
+  cards = cards.map { |card| card.join(' of ') }
+  result << cards.join(', ') << ']'
 end
 
-def display_busted(score, player_total)
-  if player_total > 21
-    prompt "You busted! Dealer wins."
-    prompt "The score is: Player #{score[0]}, Dealer #{score[1] += 1}"
-  else
-    prompt "Dealer busts! You win."
-    prompt "The score is: Player #{score[0] += 1}, Dealer #{score[1]}"
-  end
-  prompt "#{match_winner(score)} reaches 5 points first!" if match_winner(score)
-  pause
+def display_interface(player, dealer)
+  prompt "Dealer has: [#{dealer[:cards][0].join(' of ')}, ?]"
+  prompt "You have: #{read_cards(player[:cards])}"
+  prompt "Your total is: #{player[:total]}"
 end
 
-def match_winner(score)
-  if score[0] >= 5
+def match_winner(game_data)
+  if game_data[:score][:player] >= GAMES_TO_WIN
     'Player'
-  elsif score[1] >= 5
+  elsif game_data[:score][:dealer] >= GAMES_TO_WIN
     'Dealer'
   end
 end
 
-def pause
-  prompt "Press enter for next screen..."
-  gets
-  system "clear"
+def display_score(game_data, score)
+  result = detect_result(game_data[:player], game_data[:dealer])
+  if result == :player || result == :dealer_busted
+    prompt "Score: Player #{score[:player] += 1}, Dealer #{score[:dealer]}"
+  elsif result == :dealer || result == :player_busted
+    prompt "Score: Player #{score[:player]}, Dealer #{score[:dealer] += 1}"
+  else
+    prompt "Score: Player #{score[:player]}, Dealer #{score[:dealer]}"
+  end
 end
 
-def read_hand(hand)
-  temp = hand.clone
-  last = temp.pop
-  temp.join(', ') << " and #{last}"
+def display_cards(player, dealer)
+  puts "=============="
+  prompt "Dealer cards: #{read_cards(dealer[:cards])} = #{dealer[:total]}"
+  prompt "Player cards: #{read_cards(player[:cards])} = #{player[:total]}"
+  puts "=============="
 end
 
-score = [0, 0]
-prompt "Welcome to Twenty_One!"
-prompt "Find the rules at: https://bargames101.com/21-card-game-rule/"
-prompt "First to 5 wins!"
-pause
-loop do
-  deck = initialize_deck
-  player_hand = initialize_hand(deck)
-  dealer_hand = initialize_hand(deck)
-  player_total = total(player_hand)
-  dealer_total = total(dealer_hand)
-  prompt "Dealer has: #{dealer_hand[0]} and ?"
-  prompt "You have: #{read_hand(player_hand)}"
-  prompt "Your total is: #{player_total}"
+def deal_to_player(game_data, deck)
+  prompt "Dealer passes you a card"
+  deal!(game_data[:player][:cards], deck)
+  increment_total!(game_data[:player])
+  pause
+end
+
+def hit_or_stay(answer = nil)
+  prompt "Hit or stay? (h/s)"
   loop do
-    prompt "Hit or stay?"
     answer = gets.chomp.downcase
-    if answer.start_with?('h')
-      prompt "Dealer passes you a card"
-      deal!(player_hand, deck)
-      player_total = total(player_hand)
-      pause
-      if player_total > 21
-        display_hands(player_hand, dealer_hand, player_total, dealer_total)
-        display_busted(score, player_total)
-        break
-      end
-      prompt "You now have: #{read_hand(player_hand)}"
-      prompt "Your total is: #{player_total}"
-    elsif answer.start_with?('s')
-      prompt "You stay at #{player_total}"
-      prompt "Dealer Turn..."
-      loop do
-        break if dealer_total >= DEALER_STAY
-        prompt "Dealer hits"
-        deal!(dealer_hand, deck)
-        dealer_total = total(dealer_hand)
-      end
-      prompt "Dealer stays" unless dealer_total > 21
-      pause
-      display_hands(player_hand, dealer_hand, player_total, dealer_total)
-      if dealer_total > 21
-        display_busted(score, player_total)
-      else
-        display_result(score, player_total, dealer_total)
-      end
+    break if %w(h hit s stay).include?(answer)
+    prompt "Please enter hit or stay (h/s)"
+  end
+  answer
+end
+
+def player_turn(game_data, deck)
+  loop do
+    display_interface(game_data[:player], game_data[:dealer])
+    answer = hit_or_stay
+    if %w(h hit).include?(answer)
+      deal_to_player(game_data, deck)
+      break if busted?(game_data[:player])
+    elsif %w(s stay).include?(answer)
       break
-    else
-      prompt "Please enter either hit or stay"
     end
   end
-  break if match_winner(score)
+end
+
+def dealer_turn(game_data, deck)
+  prompt "You stay at #{game_data[:player][:total]}"
+  prompt "Dealer Turn..."
+  loop do
+    break if game_data[:dealer][:total] >= DEALER_STAY
+    prompt "Dealer hits"
+    deal!(game_data[:dealer][:cards], deck)
+    increment_total!(game_data[:dealer])
+  end
+  prompt "Dealer stays" unless busted?(game_data[:player])
+  pause
+end
+
+def detect_result(player, dealer)
+  if player[:total] > GAME_SIZE
+    :player_busted
+  elsif dealer[:total] > GAME_SIZE
+    :dealer_busted
+  elsif dealer[:total] < player[:total]
+    :player
+  elsif dealer[:total] > player[:total]
+    :dealer
+  else
+    :tie
+  end
+end
+
+def display_result(game_data)
+  result = detect_result(game_data[:player], game_data[:dealer])
+  case result
+  when :player_busted
+    prompt "You busted! Dealer wins."
+  when :dealer_busted
+    prompt "Dealer busted! You win."
+  when :player
+    prompt "You win!"
+  when :dealer
+    prompt "Dealer wins."
+  when :tie
+    prompt "It's a tie!"
+  end
+end
+
+def end_screen(game_data)
+  display_cards(game_data[:player], game_data[:dealer])
+  display_result(game_data)
+  display_score(game_data, game_data[:score])
+  if match_winner(game_data)
+    prompt %W(#{match_winner(game_data)} reaches #{GAMES_TO_WIN}
+              points first!).join(' ')
+  end
+end
+
+def stop_early?
   prompt "Press enter to continue or type exit to stop"
   answer = gets.chomp.downcase
-  break if answer.start_with?('e')
   system 'clear'
+  true if %w(e exit).include?(answer)
+end
+
+def play_again?(answer = nil)
+  prompt "Play again? (y/n)"
+  loop do
+    answer = gets.chomp.downcase
+    break if %w(y yes n no).include?(answer)
+    prompt "Enter yes or no (y/n)"
+  end
+  true if %w(y yes).include?(answer)
+end
+
+loop do
+  system 'clear'
+  display_introduction
+  game_data = initialize_game_data
+  loop do
+    deck = initialize_deck
+    populate!(game_data, deck)
+    player_turn(game_data, deck)
+    dealer_turn(game_data, deck) unless busted?(game_data[:player])
+    end_screen(game_data)
+    break if match_winner(game_data)
+    break if stop_early?
+  end
+  break unless play_again?
 end
 prompt "Thank you for playing Twenty One! Goodbye!"
